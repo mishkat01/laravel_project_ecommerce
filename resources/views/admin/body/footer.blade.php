900b905f8c3304dba8f9fa9dee12a05e6ef4d9c5
mishkat001 <mishkatislam007@gmail.com>
2023-10-24 12:04:35 +0600

    test added

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer bg-dark border-light-3">
+      <div class="container">
+        <div class="text-center text-white">
+          Copyright © 2018 Rukada Admin
+        </div>
+      </div>
+    </footer>
